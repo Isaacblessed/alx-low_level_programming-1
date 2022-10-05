15e6fdbaa4a480dd2beb284a63e3c2a18a9ea233
David Favour Alagwu <57830927+Alagwudavid@users.noreply.github.com>
2022-10-05 02:25:02 +0100

    Update main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,7 +3,7 @@
 
 /**
  * File: main.h
- * Auth: Ismail Mohamed
+ * Auth: Alagwu David
  * Desc: Header file containing prototypes for all functions
  *       used in the 0x0B-malloc_free directory.
  */
